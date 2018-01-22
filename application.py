c37b2bd3611fce983c08742f1962cd5ed165a544
Jenny Heddes <jenny_heddes@live.nl>
2018-01-22 18:37:58 +0000

    afbeeldingen weergegeven

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 photos = UploadSet('photos', IMAGES)
 
-app.config['UPLOADED_PHOTOS_DEST'] = 'uploads/'
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
 configure_uploads(app, photos)
 
 # ensure responses aren't cached
