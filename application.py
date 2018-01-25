d8a9efd11743ee928eaea8e30bd5c54d830c9f8d
Jenny Heddes <jenny_heddes@live.nl>
2018-01-25 12:20:26 +0000

    commments

--- application.py
+++ application.py
@@ -7,15 +7,15 @@ from datetime import datetime
 from helpers import *
 from model import *
 
-# configure application
+# Configure application
 app = Flask(__name__)
 
+# Initialise photo upload mechanism
 photos = UploadSet('photos', IMAGES)
-
 app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
 configure_uploads(app, photos)
 
-# ensure responses aren't cached
+# Ensure responses aren't cached
 if app.config["DEBUG"]:
     @app.after_request
     def after_request(response):
@@ -24,7 +24,7 @@ if app.config["DEBUG"]:
         response.headers["Pragma"] = "no-cache"
         return response
 
-# configure session to use filesystem (instead of signed cookies)
+# Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -32,14 +32,18 @@ Session(app)
 
 @app.route("/")
 def mainroute():
+    """ Returns homepage"""
     return render_template("main.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def registerroute():
+    """Allows user to register"""
 
+    # If user reached route via POST
     if request.method == "POST":
 
-        # Ensure username, password and password confirmation are filled in, otherwise apology
+        # Ensure username, password and password confirmation are filled in
+        # Otherwise it returns apology
         if not request.form.get("username"):
             return apology("register.html","Please fill in an username")
 
@@ -55,7 +59,8 @@ def registerroute():
         if not request.form.get("fullname") or " " not in request.form.get("fullname"):
             return apology("register.html","Please fill in your first and last name")
 
-        if not request.form.get("email") or "@" not in request.form.get("email") or "." not in request.form.get("email"):
+        if not request.form.get("email") \
+        or "@" not in request.form.get("email") or "." not in request.form.get("email"):
             return apology("register.html","Please fill in a valid email address")
 
         if request.form.get("work") == "I am a ...":
@@ -64,86 +69,108 @@ def registerroute():
         if request.form.get("search") == "I am looking for a ...":
             return apology("register.html","Please fill in what profession you're looking for")
 
-        check = register(request.form.get("username"), pwd_context.hash(request.form.get("password")), request.form.get("fullname"), \
-                request.form.get("work"), request.form.get("search"), request.form.get("email"))
+        # Function that puts infomation in database, ff conditions apply
+        check = register(request.form.get("username"),
+                pwd_context.hash(request.form.get("password")), \
+                request.form.get("fullname"), request.form.get("work"), \
+                request.form.get("search"), request.form.get("email"))
 
+        # Checks if username and email are not already used
         if check == "x":
             return apology("register.html", "Username already exist")
         if check == "y":
             return apology("register.html", "Email already exist")
+
+        # Remembers user id and logs in automatically
         else:
             session["user_id"] = check
         return render_template("workspace.html")
+
+    # Else if user reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def loginroute():
-    #allows the user to log in
+    """Allows the user to log in"""
 
-    session.clear()
-
+    # If user reached route via POST
     if request.method == "POST":
 
-        # Ensure username is submitted
+        # Ensures username  and password are submitted
         if not request.form.get("username"):
             return apology("login.html","Must provide username")
 
         elif not request.form.get("password"):
             return apology("login.html","Must provide valid password")
 
+        # Function that checks username and password combination, otherwise apology
         test = login(username=request.form.get("username"), hash=request.form.get("password"))
 
         if test == False:
             return apology("login.html","Invalid username and password combination")
+
+        # Redirects user to workspace if logged in correctly
         else:
             session["user_id"] = test
         return redirect(url_for("workspaceroute"))
 
+    # Else if user reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
-
 @app.route("/logout")
 def logoutroute():
-    """Log user out."""
+    """Logs user out"""
 
-    # forget any user_id
+    # Forgets any user_id
     session.clear()
 
-    # redirect user to main
+    # Redirects user to main
     return redirect(url_for("mainroute"))
 
-@app.route("/workspace", methods=["GET", "POST"])
+@app.route("/workspace", methods=["GET"])
 @login_required
 def workspaceroute():
+    """Returns workspace template"""
     return render_template("workspace.html")
 
-@app.route("/howitworks", methods=["GET", "POST"])
+@app.route("/howitworks", methods=["GET"])
 def howitworksroute():
+    """Returns howitworks template"""
     return render_template("howitworks.html")
 
 @app.route("/find", methods=["GET", "POST"])
 @login_required
 def findroute():
+    """Displays the profile of a possible match"""
+
+    # Initialises variables
     status = "temporary"
     id = session.get("user_id")
     finding=find(id)
 
+    # If users reaches route via POST
     if request.method == "POST":
 
+        # Checks if user clicked the accept or reject button
         accept = request.form.get('accept')
         reject = request.form.get('reject')
         if accept:
             status= "true"
         if reject:
             status= "false"
+
+        # Function that changes the status of the two id's
         statusupdate(id,finding,status)
+
+        # Function that checks if the id's accepted eachother and sends email
         check = statuscheck(id,finding)
         if check == True:
             inform_match(id,finding)
-
         return redirect(url_for("findroute"))
+
+    # Else if user reached route via GET (as by clicking a link or via redirect)
     else:
         if finding == 'empty':
             return apology("find.html", "no more matches available")
@@ -163,25 +190,27 @@ def profileroute():
 @app.route("/account", methods=["GET", "POST"])
 @login_required
 def accountroute():
-    # let's the user change his/her personel information
+    """Let's the user change his/her personel information"""
 
+    # If the users reaches route via POST
     if request.method == "POST":
 
-        # checks if fullname is made up of at least 2 words
+        # Checks if fullname is made up of at least 2 words
         if request.form.get("fullname"):
             if " " not in request.form.get("fullname") and len(request.form.get("fullname") < 3):
                 return apology("account.html", "please fill in your full name")
 
-        # checks if email is valid
+        # Checks if email is valid
         if request.form.get("email"):
             if "@" not in request.form.get("email") or "." not in request.form.get("email"):
                 return apology("please fill in a valid email adress")
 
-        # changes the users information, returns an integer in case of an error
-        errorcode = account(request.form.get("fullname"), request.form.get("old password"), request.form.get("password"), \
-        request.form.get("confirmpassword"), request.form.get("email"), request.form.get("work"), request.form.get("search"))
+        # Changes the users information, returns an integer in case of an error
+        errorcode = account(request.form.get("fullname"), request.form.get("old password"), \
+                    request.form.get("password"), request.form.get("confirmpassword"), \
+                    request.form.get("email"), request.form.get("work"), request.form.get("search"))
 
-        # tells the user what error occured
+        # Tells the user what error occured
         if errorcode == 0:
             return apology("account.html", "please fill in at least two words in full name")
         if errorcode == 1:
@@ -191,70 +220,85 @@ def accountroute():
         if errorcode == 3:
             return apology("account.html", "old password invalid")
 
-        # redirects the user if there was no error
+        # Redirects the user if there was no error
         else:
             return redirect(url_for("workspaceroute"))
 
+    # Else if user reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("account.html")
-
-
 
 @app.route("/upload", methods=["GET", "POST"])
 @login_required
 def uploadroute():
-    # allows the user to upload a photo to his/her profile by uploading alocal file or giving a URL
+    """Allows the user to upload a photo to his/her profile""""
+
+    # If user reached route via POST
     if request.method == "POST" and "photo"in request.files:
+
+        # Initialise id
+        id = session.get("user_id")
+
+        # Saves filename from url or from a local file
         if request.form.get("url"):
             filename = request.form.get("url")
         else:
             filename = photos.save(request.files['photo'])
-        id = session.get("user_id")
+
+        # Function that saves filename to database
         upload(filename, id)
         return redirect(url_for("profileroute"))
 
+    # Else if user reached route via GET (as by clicking a link or via redirect)id = session.get("user_id")
     else:
         return render_template("upload.html")
 
 @app.route("/delete", methods=["GET", "POST"])
 @login_required
 def deleteroute():
-    # allows the user to delete a picture from his/her profile
+    """Allows the user to delete a picture from his/her profile"""
+
+    #initialise variables
     id = session.get("user_id")
     picture = request.form.get("delete")
     selection = select(id)
 
+    # If user reached rout via POST
     if request.method == "POST":
         delete(picture, id)
         return redirect(url_for("profileroute"))
+
+    # Else if user reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("delete.html", rows = selection)
 
 @app.route("/forgotpassword", methods=["GET", "POST"])
 def forgotpasswordroute():
-    # allows the user to request a new password
+    """Allows the user to request a new password"""
 
+    # If user reached rout via POST
     if request.method == "POST":
-        # ensures the user filled in all forms
+        # Ensures the user filled in all forms
         if not request.form.get("username") or not request.form.get("email"):
             return apology("forgotpassword.html", "please fill in all fields")
         else:
-            # changes the users password in the database and sends the user an email with his/her new password, or returns an integer in case of an error
+            # Changes the users password in the database and sends the user an email
             errorcode = retrievepassword(request.form.get("username"), request.form.get("email"))
 
-        # tells the user what error occured
+        # Tells the user what error occured
         if errorcode == 0:
             return apology("forgotpassword.html", "username incorrect")
         if errorcode == 1:
             return apology("forgotpassword.html", "email incorrect")
-        # redirects the user to mail_sent.html in case of no error
+        # Redirects the user to mail_sent.html in case of no error
         else:
             return redirect(url_for("email_sentroute"))
 
+    # Else if user reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("forgotpassword.html")
 
-@app.route("/email_sent", methods=["GET", "POST"])
+@app.route("/email_sent", methods=["GET"])
 def email_sentroute():
-    # displays email_sent.html
+    """displays email_sent.html"""
     return render_template("email_sent.html")
--- model.py
+++ model.py
@@ -6,30 +6,39 @@ import smtplib
 import random
 import string
 from flask import Flask, flash, redirect, render_template, request, session, url_for
+
+# Initialises database
 db = SQL("sqlite:///WEBIK.db")
 
-
 def register(username, hash, fullname, work, search, email):
+    """Register machanism"""
 
+    # Initialise variables
     users = db.execute("SELECT * FROM users")
     usernames = [user["username"] for user in users]
     emails = [user["email"] for user in users]
 
+    # Checks if username and email already exists
     if username in usernames:
-        return x
+        return "x"
     elif email in emails:
-        return y
+        return "y"
+
+    # Puts user information into the database
     else:
         db.execute("INSERT INTO users (username, hash, fullname, work, search, email) VALUES \
                    (:username, :hash, :fullname, :work, :search, :email)", username = username, \
                     hash = hash, fullname = fullname, work = work, search = search, email = email)
 
+    # Returns users id
     rows = db.execute("SELECT * FROM users WHERE username=:username", username=username)
     id = rows[0]["id"]
     return rows[0]["id"]
 
 def login(username, hash):
+    """Login mechanism"""
 
+    # Checks username and password combination and returns False or id
     rows = db.execute("SELECT * FROM users WHERE username=:username", username=username)
     if len(rows) != 1 or not pwd_context.verify(hash, rows[0]["hash"]):
         return False
@@ -37,9 +46,9 @@ def login(username, hash):
         return rows[0]["id"]
 
 def account(fullname, oldpassword, password, confirmpassword, email, work, search):
-   # let's the user change his/her personal information
+   """Let's the user change his/her personal information"""
 
-    # changes users full name if the user submitted one
+    # Changes users full name if the user submitted one
     if fullname:
         if " " in fullname:
             db.execute("UPDATE users SET fullname = :fullname WHERE id = :id" , \
@@ -47,10 +56,11 @@ def account(fullname, oldpassword, password, confirmpassword, email, work, searc
         else:
             return 0
 
-    # changes the password if the user submitted a valid oldpassword, and a new password and a passwordconfirmation
+    # Checks if all the passwordfields are filled in otherwise apology
     if password or confirmpassword or oldpassword:
         if not oldpassword or not password or not confirmpassword:
             return 1
+        # Changes the password if the user submitted all passwords
         else:
             if password != confirmpassword:
                 return 3
@@ -61,36 +71,43 @@ def account(fullname, oldpassword, password, confirmpassword, email, work, searc
                 db.execute("UPDATE users SET hash = :hash WHERE id = :id" , \
                     hash = pwd_context.hash(password), id = session["user_id"], )
 
-    # changes the users email if the user submitted one
+    # Changes the users email if the user submitted one
     if email:
         db.execute("UPDATE users SET email = :email WHERE id = :id" , \
         email = email, id = session["user_id"], )
 
-     # changes the users profession is the user submitted one
+    # Changes the users profession is the user submitted one
     if work and work != "I am a ...":
         db.execute("UPDATE users SET work = :work WHERE id = :id" , \
         work = work, id = session["user_id"], )
 
-    # changes the users _____ if the user submitted one
+    # Changes the users search if the user submitted one
     if search and search != "I am looking for a ...":
         db.execute("UPDATE users SET search = :search WHERE id = :id" , \
         search = search, id = session["user_id"], )
 
 def profile(id):
-    # returns all photos under the users id in reverse order
+    """Returns all photos under the users id in reverse order"""
     photos = db.execute("SELECT picture FROM pictures WHERE id=:id", id=id)
     return photos[::1]
 
 def upload(filename, id):
-    # lets the user upload a new photo and adds it to the database
+    """Lets the user upload a new photo and adds it to the database"""
     db.execute("INSERT INTO pictures (id, picture) VALUES (:id, :picture)", id=id, picture=filename)
 
 def find(id):
+    """Returns a potential match id"""
+
+    # Selects search catogory from user and returns all users with the same work
     search = db.execute("SELECT search FROM users WHERE id=:id", id=id)
     possible_matches = db.execute("SELECT id FROM users WHERE work=:search", search=search[0]["search"])
     possible_matchesset = set(match["id"] for match in possible_matches)
+
+    # Selects all the users which are already seen by the user
     alreadyseen= db.execute("SELECT otherid FROM matchstatus WHERE id=:id", id=id)
     alreadyseenset= set(already["otherid"] for already in alreadyseen)
+
+    # Returns a user id from possible_matches minus the ones already seen
     show = possible_matchesset - alreadyseenset
     shows = [id for id in show]
     if shows == []:
@@ -99,77 +116,93 @@ def find(id):
         return random.choice(shows)
 
 def select(id):
-    # returns all pictures under a users id
+    """Returns all pictures from the users id"""
     picture = db.execute("SELECT * FROM pictures WHERE id=:id", id=id )
     return picture
 
 def delete(picture, id):
-    # lets the user remove a picture from the database
+    """Lets the user remove a picture from the database"""
     return db.execute("DELETE FROM pictures WHERE picture = :picture", picture = picture)
 
 def statusupdate(id,otherid,status):
+    """Inserst status into database"""
     db.execute("INSERT INTO matchstatus (id, otherid, status) VALUES (:id, :otherid, :status)", id=id, otherid=otherid, status=status)
 
 def statuscheck(id, otherid):
-    option1 = db.execute("SELECT status FROM matchstatus WHERE id=:id and otherid=:otherid", id=id, otherid=otherid)
-    option2 = db.execute("SELECT status FROM matchstatus WHERE id=:id and otherid=:otherid", id=otherid, otherid=id)
+    """Checks if two id's have a match"""
+
+    # Selects the two statuses
+    status1 = db.execute("SELECT status FROM matchstatus WHERE id=:id and otherid=:otherid", id=id, otherid=otherid)
+    status2 = db.execute("SELECT status FROM matchstatus WHERE id=:id and otherid=:otherid", id=otherid, otherid=id)
+
+    # Returns True or False depending upon mutual like or not
     try:
-        option1[0]["status"] == "true" and option2[0]["status"] == "true"
+        status1[0]["status"] == "true" and status2[0]["status"] == "true"
         return True
     except:
         return False
 
 def password_generator(chars=string.ascii_uppercase + string.digits):
-    # generate a random password with length 10, containing capital letters and numbers
+    """Generate a random password with length 10"""
     return ''.join(random.choice(chars) for i in range(10))
 
 def retrievepassword(username, email):
-    # allows the user to have a new password sent to them
+    """Allows the user to have a new password sent to them"""
+
+    # Retrieves info for username and checks if username exists
     rows = db.execute("SELECT * FROM users WHERE username=:username", username=username)
     if not rows:
          return 0
+
+    # Sends email if email is correct
     else:
+
+        # Checks if email is correct and return error code
         if email != rows[0]["email"]:
             return 1
         else:
-            # generates a new password
+            # Generates a new password
             password = password_generator()
 
-            # changes password in database
+            # Changes password in database
             db.execute("UPDATE users SET hash =:hash WHERE username =:username" , \
                     hash = pwd_context.hash(password), username = username, )
 
-            # sends new password to user
+            # Sends new password to user and sets email structure
             server = smtplib.SMTP_SSL('smtp.googlemail.com', 465)
-
             subject = "Forgot password"
             text = "Hello, {}!\n\n your new password is: \n\n '{}' \n\n you can now use this password to log into your account. \n don't forget to change youer password after logging in.".format(rows[0]["fullname"], password)
             message = 'Subject: {}\n\n{}'.format(subject, text)
 
+        # Sets up emailaccount
         server.login("tistacyhelpdesk@gmail.com", "webiktistacy")
         server.sendmail("tistacyhelpdesk@gmail.com", email, message)
 
 def inform_match(id1, id2):
-     # sends new password to user and his/her match in case of a match
+     """Sends new password to user and his/her match in case of a match"""
+
+    # Selects user and match information
     userinfo = db.execute("SELECT * FROM users WHERE id=:id", id=id1)
     matchinfo = db.execute("SELECT * FROM users WHERE id=:id", id=id2)
 
-    server = smtplib.SMTP_SSL('smtp.googlemail.com', 465)
+    # Sets up emailserver
+    server = smtplib.SMTP_SSL('smtp.googlemail.com', 465)"
 
-    subject = "You got a match"
+    # Creates a seperate  email for each person
+    subject = "You got a match
+    text1 = """ Hello, {}!\n\n Congratulations! You and {} just got a match. Here's there emailadress:
+            \ {} \n \n we hope you have a pleasant collaboration. \n \n Tistacy""".format(userinfo[0]["fullname"],
+            \matchinfo[0]["fullname"], userinfo[0]["email"])
+            message1 = 'Subject: {}\n\n{}'.format(subject, text1)
 
-    # creates a seperate user for each person
-    text1 = "Hello, {}!\n\n Congratulations! You and {} just got a match. Here's there emailadress: {} \n \n we hope you have a pleasant collaboration. \n \n Tistacy".format(userinfo[0]["fullname"], \
-        matchinfo[0]["fullname"], userinfo[0]["email"])
-    message1 = 'Subject: {}\n\n{}'.format(subject, text1)
-
-    text2 = "Hello, {}!\n\n Congratulations! You and {} just got a match. Here's there emailadress: {} \n \n we hope you have a pleasant collaboration. \n \n Tistacy".format(matchinfo[0]["fullname"], \
-        userinfo[0]["fullname"], matchinfo[0]["email"])
-    message2 = 'Subject: {}\n\n{}'.format(subject, text2)
+    text2 = """Hello, {}!\n\n Congratulations! You and {} just got a match. Here's there emailadress:
+            {} \n \n we hope you have a pleasant collaboration. \n \n Tistacy""".format(matchinfo[0]["fullname"],
+            \userinfo[0]["fullname"], matchinfo[0]["email"])
+            message2 = 'Subject: {}\n\n{}'.format(subject, text2)
 
     server.login("tistacyhelpdesk@gmail.com", "webiktistacy")
 
-    # sends each message to the corresponding user
+    # Sends each message to the corresponding user
     server.sendmail("tistacyhelpdesk@gmail.com", userinfo[0]["email"], message1)
     server.sendmail("tistacyhelpdesk@gmail.com", matchinfo[0]["email"], message2)
 
